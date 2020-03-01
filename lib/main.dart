c1a4c93279d4807c4dca7f0eb62afb4b635289b3
Jhon Christian Ambrad <jambrad123@gmail.com>
2020-03-01 23:00:03 +0800

    Implemented Initial App Scaffold.
     - AppBar with a multi-line title
     - Bottom navigation bar (Standings, Home, Schedule)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(App());
+void main() => runApp(MaterialApp(
+  home: App(),
+));
 
 class App extends StatefulWidget {
   @override
@@ -8,9 +10,84 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+
+  int _currentIndex = 1;
+
+  Widget appBarTitle(){
+    /***
+     * Renders the widget displayed in the center of the AppBar, aka the title.
+     */
+    return Column( // Use column to align the Title and the Subtitle vertically.
+      children: <Widget>[
+        Text(
+            'R&D Sportsfest 2020',
+            style: TextStyle(
+                fontWeight: FontWeight.bold
+            )
+        ),
+        RichText( // RichText to be able to customize different words.
+          text: TextSpan(
+              style: TextStyle(
+                  fontWeight: FontWeight.w300, // Makes the text slimmer
+                  fontSize: 17,
+                  color: Colors.white
+              ),
+              children: <TextSpan>[
+                TextSpan(text: "Ready. Set. "),
+                TextSpan(
+                    text: "ACCELARATE.",
+                    style: TextStyle(fontStyle: FontStyle.italic)
+                )
+              ]
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget bottomNavBar(){
+    /**
+     * Renders the bottom navigation bar of the app.
+     */
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: Colors.lightBlue[900],
+      selectedItemColor: Colors.white,
+      unselectedItemColor: Colors.white60,
+      items: <BottomNavigationBarItem> [
+        BottomNavigationBarItem(
+          title: Text("Standings"),
+          icon: Icon(Icons.assessment),
+        ),
+        BottomNavigationBarItem(
+          title: Text("Home"),
+          icon: Icon(Icons.home),
+        ),
+        BottomNavigationBarItem(
+          title: Text("Schedule"),
+          icon: Icon(Icons.calendar_today),
+        ),
+      ],
+      currentIndex: _currentIndex,
+      onTap: (index){
+        setState(() {
+          this._currentIndex = index;
+        });
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlue[900],
+        centerTitle: true,
+        title: appBarTitle()
+      ),
+      body: Container(), //Placeholder for the body. Should change accordingly.
+      bottomNavigationBar: bottomNavBar(),
+    );
   }
 }
 
