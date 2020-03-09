e4d34876fe6064b6517f272cf0a50c16a66482b3
jaomaloy <jaomaloy@gmail.com>
2020-03-10 00:24:18 +0800

    Migrate to AndroidX Support Library
    Build failure happens if material library is supported but not
    AndroidX. Hence, migrate for AndroidX support

--- android/app/src/main/java/com/youngtech/lxk_scoreboard/MainActivity.java
+++ android/app/src/main/java/com/youngtech/lxk_scoreboard/MainActivity.java
@@ -1,6 +1,6 @@
 package com.youngtech.lxk_scoreboard;
 
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
